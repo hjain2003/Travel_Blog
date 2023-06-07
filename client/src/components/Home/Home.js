24d7c649d2f8f83dc024a6ae34cebab12b9943db
Harsh <hjain_be21@thapar.edu>
2023-06-08 01:49:12 +0530

    home page, assets

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Footer from '../Footer/Footer'
+import './Home.css';
+import Navbar from '../Navbar/Navbar';
+import grass from '../Assets/grass_color.jpg'
+import cloud from '../Assets/cloud.jpg'
+
+const Home = () => {
+    return (
+        <>
+            <div className="home_container">
+                <Navbar />
+                {/* <img src={cloud} alt="" /> */}
+                <div className="home_content">
+                    <h1 align="center">Travel Blog</h1>
+                    <br />
+                    <button className='add_diary_btn'>Add a Post</button>
+                </div>
+                <div className="grass">
+                    <img src={grass} alt="" />
+                    <img src={grass} alt="" />
+                    <img src={grass} alt="" />
+                    <img src={grass} alt="" />
+                    <img src={grass} className='last_grass' alt="" />
+
+                </div>
+                <Footer />
+            </div>
+        </>
+    )
+}
+
+export default Home
